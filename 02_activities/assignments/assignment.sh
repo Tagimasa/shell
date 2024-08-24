9ae09126e7c4d4b504a15494577ebdb66a54d6e0
Tagimasa <alexei.virko@gmail.com>
2024-08-24 12:10:02 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,7 +14,7 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
-Touch dir2/file1.txt dir2/file2.txt dir2/file3.txt dir2/file4.txt dir2/file5.txt
+touch dir2/file1.txt dir2/file2.txt dir2/file3.txt dir2/file4.txt dir2/file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3.txt
 echo "Hello world" > dir2/file3.txt
